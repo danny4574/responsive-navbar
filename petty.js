8a3a5270de3400bba76c9a5b1d029eac2c7f0702
sinss <okeloadaniel07@gmail.com>
2024-11-04 16:43:10 +0100

    rated1234

---
+++ petty.js
@@ -0,0 +1,14 @@
+function showSide() {
+    const side =document.querySelector('.side');
+    side.style.display = 'flex';
+}
+showSide();
+
+
+
+function hideSide() {  
+    const side = document.querySelector('.side');
+    side.style.display = 'none';
+
+}
+hideSide();
